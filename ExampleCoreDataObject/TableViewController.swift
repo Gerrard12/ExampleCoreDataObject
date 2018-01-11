02c37b7ee239007cda27eab81eac31158e156f12
Gerardo <gsanchezc1211@gmail.com>
2018-01-11 15:52:20 -0500

    implement CodeData like objetcs

--- ExampleCoreDataObject/TableViewController.swift
+++ ExampleCoreDataObject/TableViewController.swift
@@ -7,89 +7,110 @@
 //
 
 import UIKit
+import CoreData
 
 class TableViewController: UITableViewController {
-
+    var managedObjects:[Persona] = []
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    override func viewWillAppear(_ animated: Bool) {
+        recuperarDatosCoreDate()
     }
-
     // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+        return managedObjects.count
     }
 
-    /*
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = managedObjects[indexPath.row].nombre
         return cell
     }
-    */
 
-    /*
-    // Override to support conditional editing of the table view.
     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
         return true
     }
-    */
-
-    /*
-    // Override to support editing the table view.
+    
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
+            guard let nombre = managedObjects[indexPath.row].value(forKeyPath: "nombre") as? String else {
+                return
+            }
+            eliminarRegistro(nombre: nombre)
+            recuperarDatosCoreDate()
+            tableView.reloadData()
+        }
     }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
+    
+    func getContext() -> NSManagedObjectContext {
+        let appDelegate = UIApplication.shared.delegate as? AppDelegate
+        let managedcontext = appDelegate!.persistentContainer.viewContext
+        return managedcontext
     }
-    */
 
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
+    @IBAction func IngresarAlumno(_ sender: Any) {
+        let alerta = UIAlertController(title: "Nueva Alumno", message: "Por favor agrega el nombre del nuevo alumno", preferredStyle: .alert)
+        let guardar = UIAlertAction(title: "agregar", style: .default) { (action) in
+            let texField = alerta.textFields?.first
+            self.registrarNombre(nombre: (texField?.text)!)
+            self.tableView.reloadData()
+        }
+        let cancelar = UIAlertAction(title: "cancelar", style: .cancel) { (action) in
+        }
+        alerta.addTextField { (texField:UITextField) in
+        }
+        alerta.addAction(guardar)
+        alerta.addAction(cancelar)
+        self.present(alerta, animated: true) {
+        }
     }
-    */
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    func registrarNombre(nombre: String) {
+        let managedcontext = getContext()
+        // 1. creamos una variable objeto del tipo de nuestra entidad (en este caso Persona).
+        let entity = NSEntityDescription.entity(forEntityName: "Persona", in: managedcontext) // debes de colocar el nombre de tu base de datos
+        let persona = Persona(entity: entity!, insertInto: managedcontext)
+        // 2. Registramos nuestro valor en  el managedObject
+        persona.nombre = nombre// forKeyPath: el nombre del atributo
+        // 3. utilizando nuestro managedcontext guardaremos los cambios.
+        do {
+            try managedcontext.save()
+            managedObjects.append(persona)
+        } catch let error as NSError{
+            print("No se pudo guardar, error: \(error), \(error.userInfo)")
+        }
     }
-    */
 
+    func recuperarDatosCoreDate() {
+        let managedcontext = getContext()
+        // 1. Usaremos fetch para buscar, traer y extraer.
+        let fetchRequest = NSFetchRequest<Persona>(entityName: "Persona")
+        // 2. do y catch
+        do {
+            managedObjects = try managedcontext.fetch(fetchRequest)
+        } catch let error as NSError {
+            print("No se pudo recuperar los datos, error: \(error), \(error.userInfo)")
+        }
+    }
+
+    func eliminarRegistro(nombre: String) {
+        let managedcontext = getContext()
+        let fetchRequest = NSFetchRequest<Persona>(entityName: "Persona")
+        fetchRequest.predicate = NSPredicate(format: "nombre == %@", nombre)
+        let object = try! managedcontext.fetch(fetchRequest)
+        do {
+            managedcontext.delete(object.first!)
+            try managedcontext.save()
+        } catch let error as NSError {
+            print("Error al eliminar: \(error)")
+        }
+    }
 }
